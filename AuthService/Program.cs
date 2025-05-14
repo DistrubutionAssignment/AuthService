981c61a78b975a0920a67759123be28216cf3f20
Augustvilliam <willyp@hotmail.se>
2025-05-14 15:43:02 +0200

    added jwc, roleseeding, token seeding.

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -1,17 +1,94 @@
+using System.Text;
+using AuthService.Data;
+using AuthService.Models;
+using AuthService.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DefaultCors", policy =>
+            policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+})
+    .AddEntityFrameworkStores<DataContext>()
+    .AddDefaultTokenProviders();
+
+var jwtSection = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSection["Key"]!);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = true;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSection["Issuer"],
+            ValidAudience = jwtSection["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
+
+
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 builder.Services.AddControllers();
-
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-app.MapOpenApi();
-app.UseHttpsRedirection();
+using (var scope = app.Services.CreateScope())
+{
+    var roleMgr = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
+    foreach (var role in new[] { "Admin", "User" })
+    {
+        if (!await roleMgr.RoleExistsAsync(role))
+        {
+            await roleMgr.CreateAsync(new IdentityRole(role));
+        }
+    }
+
+    var adminEmail = builder.Configuration["AdminUser:Email"]!;
+    var adminPassword = builder.Configuration["AdminUser:Password"]!;
+    if (await userMgr.FindByEmailAsync(adminEmail) == null)
+    {
+        var admin = new ApplicationUser{UserName = adminEmail,Email = adminEmail,};
+        var res = await userMgr.CreateAsync(admin, adminPassword);
+        if (res.Succeeded)
+            await userMgr.AddToRoleAsync(admin, "Admin");
+    }
+}
+
+app.UseHttpsRedirection();
+app.UseCors("DefaultCors");
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapOpenApi();
 app.MapControllers();
-
 app.Run();
